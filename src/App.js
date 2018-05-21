d62745b380f0b6d6f344a2de91be0b537b885d34
Kevin Tran <sesouk@gmail.com>
2018-05-21 12:13:37 -0700

    working on nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React, { Component } from 'react'
-import routes from './routes'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import routes from "./routes";
+import Nav from './components/nav'
 
-const Nav = (props) => <nav>
-  <ul>
-  <li>
-  <NavLink to='/'>Home</NavLink>
-  </li>
-  <li>
-  <NavLink to='/shop'>Shop</NavLink>
-  </li>
-  <li>  
-  <NavLink to='/cart'>Cart</NavLink>
-  </li>
-  </ul>
-</nav>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav/>
-        { routes }
+        <Nav />
+        {routes}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
