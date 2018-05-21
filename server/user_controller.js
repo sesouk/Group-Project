633417385b6db0dbf11f78194046d677789b3bf2
Manisha11sep <er.manisha11sep@gmail.com>
2018-05-21 13:43:47 -0700

    added query

--- server/user_controller.js
+++ server/user_controller.js
@@ -1,5 +1,5 @@
 
-module.exports ={
+module.exports = {
     getUsers: (req,res,next) =>{
         const dbInstance = req.app.get('db');
         console.log('received request')
@@ -13,8 +13,16 @@ module.exports ={
         
         dbInstance.getUserByID(userId).then(user=> res.status(200).send(user))
         .catch(error => console.log(error));
-    }
+    },
 
+    register: (req,res) =>{
+        const dbInstance = req.app.get('db');
+        console.log('received request',req.params)
+        const userId = req.params.id;
+        
+        dbInstance.getUserByID(userId).then(user=> res.status(200).send(user))
+        .catch(error => console.log(error));
+    }
 }
 
 // users: #    userid    username    useremail    usercity    userstate    userzip    userphone    useraddress
