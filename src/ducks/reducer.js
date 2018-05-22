c40a99c76e38839a84beac9762ec43ea1f14eb0b
Hunter Sexton <h.sexton@live.com>
2018-05-21 21:56:51 -0700

    cart working

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -15,7 +15,22 @@ const UPDATE_QUANTITY = 'UPDATE_QUANTITY'
 const GET_CART = 'GET_CART'
 const GET_USER_INFO = 'GET_USER_INFO'
 const GET_PRODUCTS = 'GET_PRODUCTS'
+const DECREMENT_QTY = 'DECREMENT_QTY'
+const INCREMENT_QTY = 'INCREMENT_QTY'
 
+export const decrementQty = item => {
+  return {
+    type: DECREMENT_QTY,
+    payload: item
+  }
+}
+
+export const incrementQty = item => {
+  return {
+    type: INCREMENT_QTY,
+    payload: item
+  }
+}
 export const addToCart = (item) => {
   return {
     type: ADD_TO_CART,
@@ -59,25 +74,42 @@ export const getProducts = (products) => {
 }
 
 function reducer ( state=initialState, action ){
+  let newCart = state.cart.slice()
   switch(action.type){
-
     case ADD_TO_CART:
-      return Object.assign( {}, state, { cart: action.payload })
+      let index = newCart.findIndex( e => e.item === action.payload.item.productname )
+      if(index !== -1 ){
+        newCart[index].qty +=1
+        return { ...state, cart: newCart }
+      }else{
+        return { ...state, cart: [ ...state.cart, {item: action.payload.item.productname, qty: action.payload.qty}]}
+      }
 
     case REMOVE_FROM_CART:
-      return Object.assign( {}, state, { cart: action.payload })
+      index = newCart.findIndex( e => e.item === action.payload )
+      newCart.splice(index, 1)
+      return { ...state, cart: newCart }
 
     case UPDATE_QUANTITY:
       return Object.assign( {}, state, { cart: action.payload })
 
-    case GET_CART:
-      return Object.assign( {}, state, { cart: action.payload })
+    // case GET_CART:
+    //   return state.cart
 
     case GET_USER_INFO:
       return Object.assign( {}, state, { user: action.payload })
 
     case GET_PRODUCTS:
       return Object.assign( {}, state, { products: action.payload })
+
+    case INCREMENT_QTY:
+       index = newCart.findIndex( e => e.item === action.payload )
+        newCart[index].qty +=1
+          return { ...state, cart: newCart }
+    case DECREMENT_QTY:
+       index = newCart.findIndex( e => e.item === action.payload )
+        newCart[index].qty -=1
+          return { ...state, cart: newCart }
     default:
       return state
   }
