0c240aa16287c91fc6b197dd9229a5ecf393c3c6
Kevin Tran <sesouk@gmail.com>
2018-05-22 12:42:48 -0700

    nav bar with css grid

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,7 @@ export default class Nav extends Component {
     render() {
         return (
     <nav>
-        <span><button className='nav-logo'>LOGO HERE</button></span>
+        <span className='nav-logo'><button>LOGO HERE</button></span>
         <span><Link to="/"><button>Home</button></Link></span>
         <span><Link to="/shop"><button>Shop</button></Link></span>
         <span><Link to="/cart"><button>Cart</button></Link></span>
