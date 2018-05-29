2240be4c55f266fd157ce6b17f04946f2ac04ee7
Manisha11sep <er.manisha11sep@gmail.com>
2018-05-28 19:55:11 -0700

    fixed minicart error

--- src/components/MiniCart.js
+++ src/components/MiniCart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { getProducts, removeFromCart, cartTotal} from '../ducks/reducer'
+import { getProducts, cartTotal} from '../ducks/reducer'
 import { connect } from 'react-redux'
 
 import StripeCheckout from './StripeCheckout'
@@ -68,4 +68,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {getProducts, removeFromCart, cartTotal })(Cart)
+export default connect(mapStateToProps, {getProducts, cartTotal })(Cart)
