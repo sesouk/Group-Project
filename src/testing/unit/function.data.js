7810ba26bea240e10f434d32c737f2c73117fcc2
Hunter Sexton <h.sexton@live.com>
2018-06-06 18:43:41 -0700

    testing added

---
+++ src/testing/unit/function.data.js
@@ -0,0 +1,18 @@
+module.exports = {
+  initialState:  {
+    cart:  [],
+    products: [],
+    category_items:[],
+    cart_total: 0,
+    user: {
+      name:  '',
+      email:  '',
+      address:  '',
+      phone:  '',
+      location: ''
+  },
+  product: [],
+  reducedDataItems:[],
+ 
+  }
+}
